74e88ce60dcf520fda82fe0e78ca2e690e5b9497
Mariam Babutsidze <mariam.babutsidze@wandio.com>
2020-01-27 17:01:37 +0400

    final changes

---
+++ MovieDB/Source/Core/Constants/APIKeys.swift
@@ -0,0 +1,16 @@
+//
+//  APIKeys.swift
+//  MovieDB
+//
+//  Created by Mariam Babutsidze on 1/27/20.
+//  Copyright © 2020 Maar Babu. All rights reserved.
+//
+
+import Foundation
+
+extension Constants{
+    
+    struct APIKeys{
+        static let dbKey = ""
+    }
+}
